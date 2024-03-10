94a8b4c66847665bb9dccccf9929ce88807d17fc
eshat002 <eshatjubayer22@outlook.com>
2024-03-10 17:50:55 +0600

    changed settings

--- backend/settings.py
+++ backend/settings.py
@@ -125,6 +125,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
+import os
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_ROOT = BASE_DIR / 'static_root'
